17394f6f4d655e4b461e3fe83f5df737c7cca93f
Shubham Gupta <112654423+MrShubhG1@users.noreply.github.com>
2024-08-01 21:38:00 +0530

    Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,19 +5,19 @@ export default function Footer() {
     <div>
       <hr className='hr hr-blurry w-60'/>
       <footer className="d-flex flex-wrap justify-content-between align-items-center  my-4 ">
-        <p className="col-md-4 mb-0 text-white fs-5">© Saffron</p>
+        <p className="col-md-4 mb-0 text-white fs-5">© Apna Mart</p>
 
         <ul className="nav list-group justify-content-end">
-          <li className="nav-item fs-5">Saffron Bistupur, Jamshedpur</li>
+          <li className="nav-item fs-5">Apna Mart Sai Nagar, Pune</li>
           <li className="nav-item fs-5">Open: 05:00 PM - 12:00 AM</li>
         </ul>
       </footer>
 
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <p className="col-md-4 mb-0 text-white fs-5">Developed By CodeWith@Mr.Shubh</p>
+        <p className="col-md-4 mb-0 text-white fs-5">Developed By CodeWith@PalakSatija</p>
 
         <ul className="nav list-group justify-content-end">
-          <li className="nav-item fs-5">All Rights Reserved By SHUBHAM GUPTA</li>
+          <li className="nav-item fs-5">All Rights Reserved By Palak Satija</li>
         </ul>
       </footer>
     </div>
